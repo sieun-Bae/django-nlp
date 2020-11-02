032aaf6f07f5480e9aba1a662440fec01ce23a8f
sieun-Bae <qotldms130@gmail.com>
2020-11-02 23:01:01 +0900

    report main completed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+#from report.models import reportMain
 
 def index(request):
 	return render(request, 'index.html')
+
+#def reportMain(request):
+#	return render(request, 'reportMain.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 import blog.views
+from report.views import reportMain
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blog.views.index, name='index')
+    path('', blog.views.index, name='index'),
+    path('reportMain/', reportMain, name='reportMain'),
 ]
