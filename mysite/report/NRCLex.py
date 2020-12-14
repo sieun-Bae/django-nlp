eabc83b19c134e80a5f7c318d75e8f1a3c7d2971
sieun-Bae <qotldms130@gmail.com>
2020-12-14 15:37:27 +0900

    NLP to visualize completed

---
+++ mysite/report/NRCLex.py
@@ -0,0 +1,37 @@
+import sys
+
+from nrclex import NRCLex
+
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet 
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+
+#import time
+
+#DATE = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+#f = open(f'./test/{DATE}.txt', 'r')
+
+stopword = set(stopwords.words('english'))
+score = list
+
+def preprocessing(data):
+	data = data.lower()
+	words = RegexpTokenizer(r'[a-z]+').tokenize(data)
+	words = [ w for w in words if not w in stopword ]
+
+	for pos in [wordnet.NOUN, wordnet.VERB, wordnet.ADJ, wordnet.ADV]:
+		words = [ WordNetLemmatizer().lemmatize(x, pos) for x in words ]
+
+	return " ".join(words)
+
+def processing(data):
+	lines = data
+
+	for line in lines:
+		l = preprocessing(line)
+		score.append(NRCLex(l).top_emotions) #[('anticipation', 0.3333333333333333)]
+
+# python3 NRCLex.py data
+if __name__ == '__main__':
+	processing(sys.argv[1])
--- mysite/report/visualize.py
+++ mysite/report/visualize.py
@@ -1,14 +1,45 @@
 #https://medium.com/@shistory02/django-db-data%EB%A1%9C-matplotlib-plot-render%ED%95%98%EA%B8%B0-861a8e8fb93a
+import os
+import time
+
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
+from wordcloud import WordCloud
+
 from . blog.models import Post
-import time
+
+from NRCLex import *
 
 DATE = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+directory = f"../static/img/graph/{DATE}/"
 
-def matplotlib_graph(request):
+def weekly_line(request):
 	post_df = pd.DataFrame(list(Post.objects.all().values)) 
 
 	plt.bar()
-	plt.savefig(f'../static/img/graph/{DATE}.png')
+
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	
+	plt.savefig(directory+'weekly_line.png')
+
+def monthly_line(request):
+
+def monthly_pi(request):
+
+def daily_pi(request):
+
+def wordcloud(request):
+	data = Post.objects.all()
+	data = NRCLex.processing(data)
+	wordcloud = WordCloud(max_font_size = 100).generate(data)
+
+	fig = plt.figure()
+	plt.imshow(wordcloud, interpolation = 'bilinear')
+	plt.axis('off')
+
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	
+	plt.savefig(directory+'wordcloud.png')
